b0e4b97ceaa11dc16f2ca8703bba3f3afb4fc525
dam <dam@5300101A11126>
2018-01-10 20:57:49 +0100

    He añadido libretaDirecciones.java constructor con datos de ejemplo y he inicializado la lista observable y un metodo get public

--- src/controller/LibretaDirecciones.java
+++ src/controller/LibretaDirecciones.java
@@ -10,36 +10,54 @@ import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import model.Persona;
 
 public class LibretaDirecciones extends Application {
-    
+
     private Stage escenarioPrincipal;
     private BorderPane layoutPrincipal;
     private AnchorPane vistaPersona;
+    private ObservableList datosPersona = FXCollections.observableArrayList();
+
     
+    //Datos de ejemplo
+    public LibretaDirecciones() {
+        datosPersona.add(new Persona("Manuel", "Ros Esteban"));
+        datosPersona.add(new Persona("Juan", "Pérez Martínez"));
+        datosPersona.add(new Persona("Andrea", "Chenier López"));
+        datosPersona.add(new Persona("Emilio", "González Pla"));
+        datosPersona.add(new Persona("Mónica", "de Santos Sánchez"));
+    }
+
+        //Método para devolver los datos como lista observable de personas
+    public ObservableList getDatosPersona() {
+        return datosPersona;
+    }
     @Override
     public void start(Stage escenarioPrincipal) {
-        
+
         //Debo hacerlo para que luego me funcione en l carga de escenas
         this.escenarioPrincipal = escenarioPrincipal;
-        
+
         //Establezco el título
         this.escenarioPrincipal.setTitle("Libreta de direcciones");
-        
+
         //Inicializo el layout principal
         initLayoutPrincipal();
-        
+
         //Muestro la vista persona
         muestraVistaPersona();
     }
-    
-    public void initLayoutPrincipal(){
-        
+
+    public void initLayoutPrincipal() {
+
         //Cargo el layout principal a partir de la vista VistaPrincipal.fxml
         FXMLLoader loader = new FXMLLoader();
         URL location = LibretaDirecciones.class.getResource("../view/VistaPrincipal.fxml");
@@ -49,16 +67,16 @@ public class LibretaDirecciones extends Application {
         } catch (IOException ex) {
             Logger.getLogger(LibretaDirecciones.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         //Cargo y muestro la escena que contiene ese layout principal
         Scene escena = new Scene(layoutPrincipal);
         escenarioPrincipal.setScene(escena);
         escenarioPrincipal.show();
-        
+
     }
-    
-    public void muestraVistaPersona(){
-        
+
+    public void muestraVistaPersona() {
+
         //Cargo la vista persona a partir de VistaPersona.fxml
         FXMLLoader loader = new FXMLLoader();
         URL location = LibretaDirecciones.class.getResource("../view/VistaPersona.fxml");
@@ -68,12 +86,12 @@ public class LibretaDirecciones extends Application {
         } catch (IOException ex) {
             Logger.getLogger(LibretaDirecciones.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         //Añado la vista al centro del layoutPrincipal
         layoutPrincipal.setCenter(vistaPersona);
-        
+
     }
-    
+
     //Invoco el método getPrimaryStage para que devuelva mi escenario principal
     public Stage getPrimaryStage() {
         return escenarioPrincipal;
@@ -83,5 +101,5 @@ public class LibretaDirecciones extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
